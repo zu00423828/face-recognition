d2ad8ac1cb46c028d5abf403cf547f0f0f8b5126
zu00423828 <zu00423828@gmail.com>
2021-11-27 15:34:30 +0800

    modify reqirement.txt and add facenet model

--- face_recognition.py
+++ face_recognition.py
@@ -1,106 +1,119 @@
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"   #disable gpu
+from keras import backend as K
+import time
+K.set_image_data_format('channels_first')
 import cv2
+import os
+import glob
 import numpy as np
-from numpy.linalg import norm
-import mxnet
-from collections import namedtuple
 import pandas as pd
-import dlib
-if __name__ == "__main__":
-    from tool.Symbol_MobileFace_Identification_V3 import *
-    from tool.MobileFace_Detection.mobileface_detector import MobileFaceDetection
-    from tool.mobileface_alignment import MobileFaceAlign
-else:
-    from .tool.Symbol_MobileFace_Identification_V3 import *
-    from .tool.MobileFace_Detection.mobileface_detector import MobileFaceDetection
-    from .tool.mobileface_alignment import MobileFaceAlign
+from scipy.spatial import distance
+from keras.models import load_model
+from tool.MobileFace_Detection.mobileface_detector import MobileFaceDetection
+image_size = 160
 
-class MobileFaceFeatureExtractor(object):
-    def __init__(self, model_file, epoch, batch_size, context=mxnet.cpu()):
-        self.model_file = model_file
-        self.epoch = epoch
-        self.batch_size = batch_size
-        self.context = context
+bboxes_predictor = MobileFaceDetection('model/mobilefacedet_v1_gluoncv.params', '')
+model_path = 'model/facenet_keras.h5'
+model = load_model(model_path)
+def l2_normalize(x, axis=-1, epsilon=1e-10):
 
-        network = get_feature_symbol_mobileface_v3()
-        self.model = mxnet.mod.Module(symbol=network, context=context)
-        self.model.bind(for_training=False, data_shapes=[
-                        ('data', (self.batch_size, 3, 112, 112))])
-        _, arg_params, aux_params = mxnet.model.load_checkpoint(
-            self.model_file, self.epoch)
-        self.model.set_params(arg_params, aux_params)
+    output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
 
-    def get_face_feature_batch(self, face_batch):
-        Batch = namedtuple('Batch', ['data'])
-        batch_data = np.zeros((self.batch_size, 3, 112, 112))
-        face_batch = face_batch.astype(np.float32, copy=False)
-        face_batch = (face_batch - 127.5)/127.5
-        batch_data = face_batch.transpose(0, 3, 1, 2)
-        self.model.forward(Batch([mxnet.nd.array(batch_data)]))
-        feature = self.model.get_outputs()[0].asnumpy().copy()
-        return feature
+    return output
+def prewhiten(x):
+    if x.ndim == 4:
+        axis = (1, 2, 3)
+        size = x[0].size
+    elif x.ndim == 3:
+        axis = (0, 1, 2)
+        size = x.size
+    else:
+        raise ValueError('Dimension should be 3 or 4')
+    mean = np.mean(x, axis=axis, keepdims=True)
+    std = np.std(x, axis=axis, keepdims=True)
+    std_adj = np.maximum(std, 1.0/np.sqrt(size))
+    y = (x - mean) / std_adj
+    return y
+def align_image(img):
+    bboxes = bboxes_predictor.mobileface_detector('', img)
+    if bboxes == None or len(bboxes) < 1:
+        raise Exception('not face')       
+    for bbox in bboxes:
+        xmin, ymin, xmax, ymax, _, _ = bbox
+        size_scale = 1
+        center_scale = 0.1
+        center_shift = (ymax - ymin) * center_scale
+        w_new = (ymax - ymin) * size_scale
+        h_new = (ymax - ymin) * size_scale
+        x_center = xmin + (xmax - xmin) / 2
+        y_center = ymin + (ymax - ymin) / 2 + center_shift
+        x_min = int(x_center - w_new / 2)
+        y_min = int(y_center - h_new / 2)
+        x_max = int(x_center + w_new / 2)
+        y_max = int(y_center + h_new / 2)
+        ymin=max(0,y_min)
+        y_max=min(ymax,img.shape[0])
+        x_min=max(0,x_min)
+        y_max=min(ymax,img.shape[0])
+        x_max=min(x_max,img.shape[1])
+        face=img[y_min:y_max,x_min:x_max]
+        aligned=cv2.resize(face,(160,160))
+        return aligned
+            
+    # cascade = cv2.CascadeClassifier(cascade_path)
+
+    # faces = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=3)
+
+    # if(len(faces)>0):
+    #     (x, y, w, h) = faces[0]
+    #     face = img[y:y+h, x:x+w]
+    #     faceMargin = np.zeros((h+margin*2, w+margin*2, 3), dtype = np.uint8)
+    #     faceMargin[margin:margin+h, margin:margin+w] = face
+    #     aligned = cv2.resize(faceMargin, (image_size, image_size))
+    #     return aligned
+    # else:
+    #     print('is not face')
+    #     return None
 
 class FaceRecognition():
     def __init__(self, model_dir, threshold):
-        self.face_feature_extractor = MobileFaceFeatureExtractor(
-            f'{model_dir}/MobileFace_Identification_V3', 0, 1, mxnet.cpu())
-        self.bboxes_predictor = MobileFaceDetection(f'{model_dir}/mobilefacedet_v1_gluoncv.params', '')
-        self.landmark_predictor = dlib.shape_predictor(f'{model_dir}/mobileface_landmark_emnme_v1.dat')
-        self.align_tool = MobileFaceAlign(f'{model_dir}/mobileface_align_v1.npy')
+
         self.threshold = threshold
-    def face_align(self,img_path): 
-        '''
-            get 112*112 face image is alignment
-        '''
-        align_size = (112, 112) 
-        img_mat = cv2.imread(img_path)
-        bboxes = self.bboxes_predictor.mobileface_detector(img_path, img_mat)
-        if bboxes == None or len(bboxes) < 1:
-            raise Exception('not face')       
-        for bbox in bboxes:
-            xmin, ymin, xmax, ymax, _, _ = bbox
-            size_scale = 0.75
-            center_scale = 0.1
-            center_shift = (ymax - ymin) * center_scale
-            w_new = (ymax - ymin) * size_scale
-            h_new = (ymax - ymin) * size_scale
-            x_center = xmin + (xmax - xmin) / 2
-            y_center = ymin + (ymax - ymin) / 2 + center_shift
-            x_min = int(x_center - w_new / 2)
-            y_min = int(y_center - h_new / 2)
-            x_max = int(x_center + w_new / 2)
-            y_max = int(y_center + h_new / 2)
-            dlib_box = dlib.rectangle(x_min, y_min, x_max, y_max)
-            shape = self.landmark_predictor(img_mat, dlib_box).parts()
-            points = [[p.x,p.y] for p in shape]
-            align_result = self.align_tool.get_align(img_mat, [points], align_size)
-            return align_result[0]
+
     def get_feature(self, img_paths):  
         '''
-            get 1*256 face vetor
+            get 128 face vetor
         '''
+        from tqdm import tqdm
         feature_list = []
-        for item in img_paths:
-            img = self.face_align(item)
-            face_batch = np.array([img])
-            feature = self.face_feature_extractor.get_face_feature_batch(face_batch
-                )
-            feature[0] -= np.mean(feature[0])
-            feature_list.append({'filename': item, 'feature': feature[0]})
+        for item in tqdm(img_paths):
+            img=cv2.imread(item)
+            # img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+            try:
+                img=align_image(img)
+                white_img=prewhiten(img)
+                white_img= white_img[np.newaxis,:]
+                feature = l2_normalize(np.concatenate(model.predict(white_img)))
+                feature_list.append({'filename': item, 'feature': feature})
+            #     img=cv2.imread(item)
+            #     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            #     faces = self.face_cascade.detectMultiScale(gray, 1.3, 5)
+            #     for (x, y, w, h) in faces:
+            #         x1 = x-PADDING
+            #         y1 = y-PADDING
+            #         x2 = x+w+PADDING
+            #         y2 = y+h+PADDING
+            #         img = cv2.rectangle(img,(x1, y1),(x2, y2),(255,0,0),2)
+            #         feature = img_path_to_encoding(item, self.FRmodel)
+
+            #     feature_list.append({'filename': item, 'feature': feature})
+            except Exception as  e:
+                print(item,e)
+        # print(feature_list)
         return feature_list
 
-    def get_cosine_similarity(self, default_feature, new_feature):
-        # result=np.dot(default_feature, new_feature) / (norm(default_feature) * norm(new_feature)) # one to one
-        result = default_feature.dot(
-            new_feature) / (norm(default_feature, axis=1) * norm(new_feature))  # many to one
-        return result
 
-    def get_euclidean_distance(self, feature_1, feature_2):
-        # feature_1 = np.array(feature_1)
-        # feature_2 = np.array(feature_2)
-        # dist = 1/(1+dist)
-        # dist=norm(feature_1-feature_2)
-        dist = np.sqrt(np.sum(np.square(feature_1 - feature_2)))
-        return dist
 
 
     def compare_similarity(self, people_data, img):
@@ -111,35 +124,52 @@ class FaceRecognition():
         people_name = df['filename'].tolist()
         people_feature = np.array(df['feature'].tolist())
         compelte = []
-        img_feature = self.get_feature([img])[0]["feature"]
-        # cosine_similarity_scores = self.get_cosine_similarity(
-        #     people_feature, img_feature)
-        # print(cosine_similarity_scores)
-    
-        for (person_name, person_feature) in zip(people_name, people_feature):
-            dist = self.get_euclidean_distance(person_feature, img_feature)
-            print(person_name,dist)
-            if dist < self.threshold:
-                compelte.append({'photoID': person_name, 'confidence': 1-dist})
+        try:
+            img_feature = self.get_feature([img])[0]['feature']
+
+            # print(cosine_similarity_scores)
+            # print('min',np.min(cosine_similarity_scores),'max',np.max(cosine_similarity_scores))
+            min_dist=100
+            filename=''
+            for (person_name, person_feature) in zip(people_name, people_feature):
+                # dist = np.linalg.norm(person_feature.reshape(-1,1) - img_feature.reshape(-1,1),ord=2)
+                dist=distance.euclidean(person_feature, img_feature)
+                print(person_name,dist)
+                if dist < min_dist:
+                    min_dist=dist
+                    filename=person_name
+                if  dist <self.threshold :
+                    compelte.append({'photoID': person_name, 'confidence': dist})
+                # compelte.append({'photoID': person_name, 'confidence': dist})
+        except Exception as e:
+            print('compare img is not face')
+            return []
+        print('min_dist',filename,min_dist)
         return compelte
 
 
 def test_time():
-    facerecognition.compare_similarity(people_data,a2)
+    facerecognition.compare_similarity(people_data,filelist[-1])
 
 if __name__ == "__main__":
+    from glob import glob
+    from random import shuffle
+    from time import time
     model_dir = 'model'
-    facerecognition = FaceRecognition(model_dir, 0.5)
-    a2 = 'test_data/101.png'
-    filelist = ['test_data/3.png','test_data/101.png','test_data/0.png'
-                ]
-    people_data = facerecognition.get_feature(filelist)# get people feature
+    facerecognition = FaceRecognition(model_dir, 0.7)
+    filelist=glob('test_data/face_data/*')#face_data/*')
+    shuffle(filelist)
+    people_data = facerecognition.get_feature(filelist[:500])# get people feature
+    print('comparefile','test_data/101.png')
+    print(people_data[0]['feature'].shape)
+    st=time()
+    # facerecognition.compare_similarity(people_data, 'test_data/e1.png')
+    print(facerecognition.compare_similarity(people_data,  'test_data/101.png')) # similarity
+    et=time()
+    print('cost:',f'{et-st:0.8f} s')
 
-    print(facerecognition.compare_similarity(people_data, a2)) # similarity
-
-
-    from timeit import timeit
-    print()
-    number=100000
-    t=timeit('test_time','from __main__ import test_time',number=number)
-    print('average_cost:',t/number)
+    # from timeit import timeit
+    # print()
+    # number=100000
+    # t=timeit('test_time','from __main__ import test_time',number=number)
+    # print('average_cost:',t/number)
