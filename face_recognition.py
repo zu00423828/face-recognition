9a266eead66f5ed0b22a78a90e5d94e6f30dff37
yuan <zu00423828@gmail.com>
2022-05-20 08:25:09 +0800

    modify graph deafult

--- face_recognition.py
+++ face_recognition.py
@@ -1,25 +1,26 @@
+import tensorflow as tf
+import json
+from keras.models import load_model
+from scipy.spatial import distance
+import pandas as pd
+import numpy as np
+import glob
+import cv2
+import time
 import os
 import dlib
-os.environ["CUDA_VISIBLE_DEVICES"] = "-1"   #disable gpu
-import time
-import cv2
-import os
-import glob
-import numpy as np
-import pandas as pd
-from scipy.spatial import distance
-from keras.models import load_model
-import json
-import tensorflow as tf
-if __name__== '__main__':
+os.environ["CUDA_VISIBLE_DEVICES"] = "-1"  # disable gpu
+if __name__ == '__main__':
     from tool.face_align import FaceAligner
 else:
     from .tool.face_align import FaceAligner
 image_size = 160
-detector=None
-fa=None
+detector = None
+fa = None
 feature_extractor = None
-grpah = None
+graph = tf.get_default_graph()
+
+
 def load_pretrain_model(model_dir):
     '''
     load 預訓練的模型 
@@ -29,17 +30,21 @@ def load_pretrain_model(model_dir):
     global detector
     global feature_extractor
     global fa
-    global graph
     # bboxes_predictor = MobileFaceDetection(f'{model_dir}/mobilefacedet_v1_gluoncv.params', '')
     detector = dlib.get_frontal_face_detector()
-    predictor = dlib.shape_predictor( f'{model_dir}/shape_predictor_5_face_landmarks.dat')
-    fa=FaceAligner(predictor)
+    predictor = dlib.shape_predictor(
+        f'{model_dir}/shape_predictor_5_face_landmarks.dat')
+    fa = FaceAligner(predictor)
     feature_extractor = load_model(f'{model_dir}/facenet_keras.h5')
-    graph =tf.get_default_graph()
+
+
 def l2_normalize(x, axis=-1, epsilon=1e-10):
 
-    output = x / np.sqrt(np.maximum(np.sum(np.square(x), axis=axis, keepdims=True), epsilon))
+    output = x / np.sqrt(np.maximum(np.sum(np.square(x),
+                         axis=axis, keepdims=True), epsilon))
     return output
+
+
 def prewhiten(x):
     '''
     圖片白化
@@ -59,6 +64,7 @@ def prewhiten(x):
     y = (x - mean) / std_adj
     return y
 
+
 def face_align(filename):
     '''
     人臉校正
@@ -70,12 +76,14 @@ def face_align(filename):
     faces = detector(gray, 1)
     for face in faces:
         faceAligned = fa.align(img, gray, face)
-        faceAligned=cv2.cvtColor(faceAligned,cv2.COLOR_BGR2RGB)
-        faceAligned=cv2.resize(faceAligned,(160,160))
+        faceAligned = cv2.cvtColor(faceAligned, cv2.COLOR_BGR2RGB)
+        faceAligned = cv2.resize(faceAligned, (160, 160))
         return faceAligned
-    img=cv2.resize(img,(160,160))
-    img=cv2.cvtColor(img,cv2.COLOR_BGR2RGB)
+    img = cv2.resize(img, (160, 160))
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     return img
+
+
 class FaceRecognition():
     '''
     參數
@@ -87,14 +95,16 @@ class FaceRecognition():
     threshold 是信心閥值 愈低愈好 預設為0.7 \n
     sep 是取得feature的檔案分割符號 用於做 label
     '''
-    def __init__(self, model_dir=None, threshold=0.7,sep='.'):
+
+    def __init__(self, model_dir=None, threshold=0.7, sep='.'):
         self.threshold = threshold
-        self.sep=sep
+        self.sep = sep
         if model_dir is None:
-            model_dir= f"{os.path.split(os.path.realpath(__file__))[0]}/model"
+            model_dir = f"{os.path.split(os.path.realpath(__file__))[0]}/model"
         if detector is None:
             load_pretrain_model(model_dir)
-    def get_feature(self, img_paths):  
+
+    def get_feature(self, img_paths):
         '''
             get 128 face vetor
         '''
@@ -102,17 +112,19 @@ class FaceRecognition():
         feature_list = []
         for item in tqdm(img_paths):
             try:
-                name=os.path.basename(item).split(self.sep)[0]
-                img=face_align(item)
-                white_img=prewhiten(img)
-                white_img= white_img[np.newaxis,:]
+                name = os.path.basename(item).split(self.sep)[0]
+                img = face_align(item)
+                white_img = prewhiten(img)
+                white_img = white_img[np.newaxis, :]
                 with graph.as_default():
-                    feature = l2_normalize(np.concatenate(feature_extractor.predict(white_img)))
+                    feature = l2_normalize(np.concatenate(
+                        feature_extractor.predict(white_img)))
                     feature_list.append({'label': name, 'feature': feature})
-            except Exception as  e:
-                print(item,e)
+            except Exception as e:
+                print(item, e)
         # print(feature_list)
         return feature_list
+
     def compare_similarity(self, people_data, img_path):
         '''
             people feature and new_img make similarity
@@ -126,22 +138,23 @@ class FaceRecognition():
 
             # print(cosine_similarity_scores)
             # print('min',np.min(cosine_similarity_scores),'max',np.max(cosine_similarity_scores))
-            min_dist=100
-            filename=''
+            min_dist = 100
+            filename = ''
             for (person_name, person_feature) in zip(people_name, people_feature):
                 # dist = np.linalg.norm(person_feature.reshape(-1,1) - img_feature.reshape(-1,1),ord=2)
-                dist=distance.euclidean(person_feature, img_feature)
+                dist = distance.euclidean(person_feature, img_feature)
                 # print(person_name,dist)
                 if dist < min_dist:
-                    min_dist=dist
-                    filename=person_name
-                if  dist <self.threshold :
-                    compelte.append({'photoID': person_name, 'confidence': dist})
+                    min_dist = dist
+                    filename = person_name
+                if dist < self.threshold:
+                    compelte.append(
+                        {'photoID': person_name, 'confidence': dist})
                 # compelte.append({'photoID': person_name, 'confidence': dist})
         except Exception as e:
             print('compare img is not face')
             return json.dumps([])
-        print('min_dist',filename,min_dist)
+        print('min_dist', filename, min_dist)
         return json.dumps(compelte)
 
 
@@ -150,27 +163,26 @@ if __name__ == "__main__":
     from random import shuffle
     from time import time
     model_dir = 'model'
-    facerecognition = FaceRecognition(model_dir = None, threshold = 0.7, sep = '.')
-    filelist=glob('face_data/*')#face_data/*')
+    facerecognition = FaceRecognition(model_dir=None, threshold=0.7, sep='.')
+    filelist = glob('face_data/*')  # face_data/*')
     shuffle(filelist)
 
-
-    #取得 feature 和 label
-    people_data = facerecognition.get_feature(filelist[:500])# get people feature
-
-
+    # 取得 feature 和 label
+    people_data = facerecognition.get_feature(
+        filelist[:500])  # get people feature
 
     from glob import glob
 
     # facerecognition.compare_similarity(people_data, 'test_data/e1.png')
-    img_list=glob('test_data/*')
+    img_list = glob('test_data/*')
     img_list.sort()
     for img in img_list:
-        st=time()
-        #取得 辨識結果
-        print(facerecognition.compare_similarity(people_data,  img)) # similarity
-        et=time()
-        print('cost:',f'{et-st:0.8f} s')
+        st = time()
+        # 取得 辨識結果
+        print(facerecognition.compare_similarity(
+            people_data,  img))  # similarity
+        et = time()
+        print('cost:', f'{et-st:0.8f} s')
         print()
 
     # from timeit import timeit
